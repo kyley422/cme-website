40ff3135c48589d453c3307f6b4323b241e614af
Kyle Yang <dev.kyleyang@gmail.com>
2025-03-04 04:21:48 -0800

    change mobile detection to react-responsive

--- src/pages/programs.js
+++ src/pages/programs.js
@@ -4,27 +4,11 @@ import ProgramBanner from 'components/ProgramBanner';
 import ProgramCalendar from 'components/ProgramCalendar';
 import ProgramCalendarMobile from 'components/ProgramCalendarMobile';
 import React, {useState,useEffect} from 'react';
+import { useMediaQuery } from 'react-responsive';
 import {Box} from '@chakra-ui/react';
 
-function useWindowSize() {
-  const [size, setSize] = useState({ width: window.innerWidth, height: window.innerHeight });
-
-  useEffect(() => {
-    function handleResize() {
-      setSize({ width: window.innerWidth, height: window.innerHeight });
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return size;
-}
-
-
 function Programs() {
-  const { width } = useWindowSize();
-  const isMobile = width < 768;
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
 
   return (
     <Box>
