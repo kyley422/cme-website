123b17af8858e15852d4a3552948b8c3a7e9a127
Kyle Yang <dev.kyleyang@gmail.com>
2025-03-04 01:39:16 -0800

    performances mobile

--- src/components/PastPerformance.js
+++ src/components/PastPerformance.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import { Box, Flex, Grid, Heading, Image, Text, useColorModeValue} from '@chakra-ui/react'
+import { Box, Center, Flex, Grid, Heading, Image, Text, useColorModeValue} from '@chakra-ui/react'
 
 const PastPerformance = () => {
 
@@ -61,38 +61,44 @@ const PastPerformance = () => {
         <Flex 
         direction="column"
         width="full" 
-        paddingBottom="36px"
-        paddingLeft="10%"
-        paddingRight="10%"
+        paddingTop={{ base: "24px", md: "36px" }}
+        paddingBottom={{ base: "24px", md: "36px" }}
+        paddingLeft={{ base: "5%", md: "10%" }}
+        paddingRight={{ base: "5%", md: "10%" }}
         gap={5}
         bg={useColorModeValue('white', 'black')}
         >
-            <Heading fontWeight="bold" fontSize="40px">Past Performances</Heading>
+            <Heading fontWeight="bold" fontSize={{ base: "2xl", md: "40px" }} textAlign="center">Past Performances</Heading>
         </Flex>
 
         <Grid
-        paddingLeft="6%"
-        paddingBottom="200px"
-        templateColumns="repeat(3, 1fr)"
-        gap={2}
+        // paddingLeft={{ base: "5%", md: "6%" }}
+        paddingBottom={{ base: "50px", md: "200px" }}
+        templateColumns={{
+          base: "repeat(1, 1fr)",
+          sm: "repeat(2, 1fr)",
+          md: "repeat(3, 1fr)"
+        }}
+        gap={4}
         width="full"
         marginTop="20px"
         justifyItems="center"
         >
           {past_performances.map((perf, idx) => (
-            <Box key={idx} textAlign="center" maxWidth="300px">
-              <Link href={perf.link} isExternal>
+            <Box key={idx} textAlign="center" maxWidth="300px" marginBottom={{ base: "20px", md: "0" }}>
+              <Link href={perf.link} target="_blank">
                 <Image
                 src={perf.image}
                 alt={perf.name}
-                height="270px"
+                height={{ base: "200px", md: "270px" }}
                 width="100%"
                 objectFit="cover"
                 mb="8px"
                 _hover={{ transform: 'scale(1.05)' }}
+                transition="transform 0.3s ease-in-out"
                 />
               </Link>
-              <Text fontWeight="medium" fontSize="20px" paddingTop="5px">{perf.name}</Text>
+              <Text fontWeight="medium" fontSize={{ base: "md", md: "20px" }} paddingTop="5px">{perf.name}</Text>
             </Box>
           ))}
         </Grid>
