eaed865017b25d794b016e29672ae9299d0fbe80
Kye Shi <git@kye.sh>
2025-05-02 00:27:19 -0700

    add cme logo and adjust layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,9 +1,12 @@
 import * as Tabler from '@tabler/icons-react';
+import Image from 'next/image';
 import { redirect } from 'next/navigation';
 import type * as React from 'react';
 
 import NavList from 'components/admin/NavMenu';
 import * as Session from 'server/session';
+
+import logo from './logo.svg';
 
 const NavHeading: React.FC<{ children: string }> = (props) => (
   <h2 className="uppercase font-medium text-light">{props.children}</h2>
@@ -17,15 +20,20 @@ export default async function AdminLayout(props: {
 
   return (
     <div className="grid grid-rows-[auto_1fr] grid-cols-[auto_1fr]">
-      <header className="col-span-2 p-8">
-        <h1 className="text-xl font-medium">UCLA Chinese Music Ensemble</h1>
+      <header className="p-8 grid grid-cols-[auto_1fr] items-center">
+        <Image src={logo} alt="hi" className="w-32 -ml-3 -mr-3" />
+        <h1 className="text-2xl font-medium w-48">
+          UCLA Chinese Music Ensemble
+        </h1>
       </header>
-      <nav className="p-8 flex flex-col gap-4">
+      <nav className="p-8 flex flex-col gap-4 col-start-1">
         <NavHeading>pages</NavHeading>
         <NavList />
         <NavHeading>account</NavHeading>
       </nav>
-      <main>{props.children}</main>
+      <main className="row-start-1 col-start-2 row-span-2 p-8">
+        {props.children}
+      </main>
     </div>
   );
 }
